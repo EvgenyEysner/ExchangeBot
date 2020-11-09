074b08896f027f93569e0b865b13ec42fcff5279
M@nk3y1980 <eugen.eisner@gmail.com>
2020-11-09 07:08:13 +0100

    Install lxml-4.6.1

--- telegram.py
+++ telegram.py
@@ -1,4 +1,7 @@
 import telebot
+import requests
+import lxml.html
+from lxml import etree
 
 
 Token = '1473822720:AAEj6nJvTY0PY2Qpy1peXahsQAqN14sbtUo'
@@ -8,6 +11,11 @@ bot = telebot.TeleBot(Token)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message: telebot.types.Message):
     bot.reply_to(message, f'Доброго времени суток! {message.chat.username}')
+
+
+@bot.message_handler(content_types=['photo'])
+def like_pictures(message: telebot.types.Message):
+    bot.reply_to(message, f'Nice meme XDD!')
 
 
 bot.polling(none_stop=True)
