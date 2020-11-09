245d71fc3f3c9aef3d0cffd51c35dbb05a9358ca
M@nk3y1980 <eugen.eisner@gmail.com>
2020-11-09 06:28:35 +0100

    Add welcome Function

--- telegram.py
+++ telegram.py
@@ -1,3 +1,13 @@
 import telebot
 
-Token = '1492678585:AAF3iMnKwgbtg7z6PYLBC2JEDMnPvPMvZcM'
+
+Token = '1473822720:AAEj6nJvTY0PY2Qpy1peXahsQAqN14sbtUo'
+bot = telebot.TeleBot(Token)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message: telebot.types.Message):
+    bot.reply_to(message, f'Доброго времени суток! {message.chat.username}')
+
+
+bot.polling(none_stop=True)
