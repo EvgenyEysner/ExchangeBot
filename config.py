9a109ebfb6fdc49d218b8dad25456f815acf83b1
M@nk3y1980 <eugen.eisner@gmail.com>
2020-11-18 10:03:01 +0100

    edit config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 TOKEN = '1473822720:AAEj6nJvTY0PY2Qpy1peXahsQAqN14sbtUo'
 
-keys = {'ДОЛЛАР': 'USD',
+keys = {
+        'ДОЛЛАР': 'USD',
         'ЕВРО': 'EUR',
         'РУБЛЬ': 'RUB',
         'ЗЛОТЫ': 'PLN',
         'КРОНА': 'CZK',
         }
-
